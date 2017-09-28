a730f1d15ce34eb78e84b2725e6e1c0880653c30
maldad <canond_7@hotmail.com>
2017-09-28 13:14:54 -0500

    accesing arrays with negative indexes D:

---
+++ zen/arrays.py
@@ -0,0 +1,11 @@
+a = [1,2,3,4,5]
+print "array is: ", a
+print "you can access an array using a positive index"
+print "a[0] is: ", a[0]
+print "a[1] is: ", a[1]
+print "a[2] is: ", a[2]
+
+print "or with a negative index"
+print "a[-1] is: ", a[-1]
+print "a[-2] is: ", a[-2]
+print "a[-3] is: ", a[-3]
